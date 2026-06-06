6793586bd68e25a0afc4770715b49fdcbbfdd9cd
Youssef <youssefabdallah2016@gmail.com>
2026-06-06 22:45:23 +0300

    - add routes of screens - add app router of all app screen - create empty onBoarding view

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/routing/routes.dart';
+
+import '../../features/onboarding/onboarding_screen.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => OnBoardingScreen());
+
+      default:
+        return null;
+    }
+  }
+}
