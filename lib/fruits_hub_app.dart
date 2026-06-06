95d7b9ec1d35355a3b9ad651f80dda66f35ca692
Youssef <youssefabdallah2016@gmail.com>
2026-06-06 22:25:16 +0300

    - separate main from user app

---
+++ lib/fruits_hub_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FruitsHubApp extends StatelessWidget {
+  const FruitsHubApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
