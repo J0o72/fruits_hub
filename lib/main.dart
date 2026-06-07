b9a970993117399fcc6e3b7296bd08322cfdbc9a
Youssef <youssefabdallah2016@gmail.com>
2026-06-07 18:51:33 +0300

    fake push

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/routing/app_router.dart';
 import 'package:fruit_hub/fruits_hub_app.dart';
 
+// fake push
 void main() {
   runApp(FruitsHubApp(appRouter: AppRouter()));
 }
