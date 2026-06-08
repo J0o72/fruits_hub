e0b044d8d7950c69296b6ad6f9df4d6f73dd346f
Youssef <youssefabdallah2016@gmail.com>
2026-06-08 23:40:23 +0300

    - add localization into app & make app (RTL) arabic

--- lib/fruits_hub_app.dart
+++ lib/fruits_hub_app.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_hub/core/routing/app_router.dart';
@@ -15,10 +16,12 @@ class FruitsHubApp extends StatelessWidget {
       minTextAdapt: true,
       child: MaterialApp(
         title: 'Fruits Hub App',
-
         initialRoute: Routes.onBoardingScreen,
         onGenerateRoute: appRouter.generateRoute,
         debugShowCheckedModeBanner: false,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/core/routing/app_router.dart';
 import 'package:fruit_hub/fruits_hub_app.dart';
 
 // fake push
-void main() {
-  runApp(FruitsHubApp(appRouter: AppRouter()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('ar'),
+      startLocale: Locale('ar'),
+      child: FruitsHubApp(appRouter: AppRouter()),
+    ),
+  );
 }
