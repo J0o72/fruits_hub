0ee8cb4009fe4aad1eb43f840e54d801c9a0353a
Youssef <youssefabdallah2016@gmail.com>
2026-06-11 16:38:12 +0300

    - add flavors into FruitHub (Android only) - add configuration of app launcher - main become one for dev & one for Prod

---
+++ lib/main_development.dart
@@ -0,0 +1,24 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fruit_hub/core/helpers/shared_pref_singleton.dart';
+import 'package:fruit_hub/core/routing/app_router.dart';
+import 'package:fruit_hub/fruits_hub_app.dart';
+
+// fake push
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  await SharedPrefSingleton.init();
+  await ScreenUtil.ensureScreenSize();
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('ar'),
+      startLocale: Locale('ar'),
+      child: FruitsHubApp(appRouter: AppRouter()),
+    ),
+  );
+}
