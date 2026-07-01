a3c505843c4ec19751ca3df2cdf95d93a6e30967
Youssef <youssefabdallah2016@gmail.com>
2026-07-01 17:38:06 +0300

    - create error class to handle any errors of incoming backend - create firebase error handler - create firebase auth service to contains all methods that belongs to auth - create auth repo

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,1 +1,10 @@
-abstract class AuthRepo {}
+import 'package:fruit_hub/features/auth/domain/entities/user_entity.dart';
+
+import '../../../../core/networking/result.dart';
+
+abstract class AuthRepo {
+  Future<Result<UserEntity>> createUserWithEmailAndPassword(
+    String email,
+    String password,
+  );
+}
