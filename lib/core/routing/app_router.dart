15ce7bfed25e8aeb8c96dffa1699ebe470828261
Youssef <youssefabdallah2016@gmail.com>
2026-07-01 22:46:54 +0300

    provide register cubit into ui using  app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/DI/dependancy_injection.dart';
 import 'package:fruit_hub/core/routing/routes.dart';
+import 'package:fruit_hub/features/auth/logic/register/register_cubit.dart';
 import 'package:fruit_hub/features/auth/ui/forget_password/forget_password_screen.dart';
 import 'package:fruit_hub/features/auth/ui/register/register_screen.dart';
 
@@ -16,7 +19,12 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => LoginScreen());
 
       case Routes.registerScreen:
-        return MaterialPageRoute(builder: (_) => RegisterScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<RegisterCubit>(),
+            child: RegisterScreen(),
+          ),
+        );
 
       case Routes.forgetPasswordScreen:
         return MaterialPageRoute(builder: (_) => ForgetPasswordScreen());
