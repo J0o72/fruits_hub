f192c6490fc831633e01a563a5d1f68f063031ba
Youssef <youssefabdallah2016@gmail.com>
2026-07-03 05:44:41 +0300

    implement login method inside login cubit

--- lib/features/auth/logic/login/login_cubit.dart
+++ lib/features/auth/logic/login/login_cubit.dart
@@ -1,6 +1,34 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruit_hub/features/auth/logic/login/login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit({required this.authRepo}) : super(LoginInitial());
+
+  final AuthRepo authRepo;
+
+  Future<void> loginUsingEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    emit(LoginLoading());
+
+    final response = await authRepo.loginWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    response.fold(
+      (error) {
+        emit(
+          LoginFailure(
+            message:
+                error.message ?? 'لقد حدث خطأ ما. الرجاء المحاولة مرة اخرى.',
+          ),
+        );
+      },
+      (userEntity) {
+        emit(LoginSuccess(userEntity: userEntity));
+      },
+    );
+  }
 }
