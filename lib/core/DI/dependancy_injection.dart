8a805625546233c0bd6ce29669a986c5ae5f2bbe
Youssef <youssefabdallah2016@gmail.com>
2026-07-03 05:46:24 +0300

    update DI with login cubit

--- lib/core/DI/dependancy_injection.dart
+++ lib/core/DI/dependancy_injection.dart
@@ -1,6 +1,7 @@
 import 'package:fruit_hub/core/networking/firebase_auth_service.dart';
 import 'package:fruit_hub/features/auth/data/repos/auth_repo_implementation.dart';
 import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruit_hub/features/auth/logic/login/login_cubit.dart';
 import 'package:fruit_hub/features/auth/logic/register/register_cubit.dart';
 import 'package:get_it/get_it.dart';
 
@@ -8,14 +9,19 @@ final getIt = GetIt.instance;
 
 Future<void> setUpGetIt() async {
   getIt.registerSingleton<FirebaseAuthService>(FirebaseAuthService());
-
-  // Register
   getIt.registerLazySingleton<AuthRepo>(
     () => AuthRepoImplementation(
       firebaseAuthService: getIt<FirebaseAuthService>(),
     ),
   );
+
+  // Register
   getIt.registerFactory<RegisterCubit>(
     () => RegisterCubit(authRepo: getIt<AuthRepo>()),
+  );
+
+  // Login
+  getIt.registerFactory<LoginCubit>(
+    () => LoginCubit(authRepo: getIt<AuthRepo>()),
   );
 }
