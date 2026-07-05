adc6a1306ae18de4c33435159679885f90fdb48b
Youssef <youssefabdallah2016@gmail.com>
2026-07-05 18:16:02 +0300

    add sign in with facebook implement... to firebaseAuthService

--- lib/core/networking/firebase_auth_service.dart
+++ lib/core/networking/firebase_auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class FirebaseAuthService {
@@ -35,6 +36,20 @@ class FirebaseAuthService {
 
     final credential = GoogleAuthProvider.credential(
       idToken: googleAuth.idToken,
+    );
+
+    return (await FirebaseAuth.instance.signInWithCredential(credential)).user!;
+  }
+
+  Future<User> signInWithFacebook() async {
+    final result = await FacebookAuth.instance.login();
+
+    if (result.status != LoginStatus.success) {
+      throw Exception(result.message);
+    }
+
+    final credential = FacebookAuthProvider.credential(
+      result.accessToken!.tokenString,
     );
 
     return (await FirebaseAuth.instance.signInWithCredential(credential)).user!;
