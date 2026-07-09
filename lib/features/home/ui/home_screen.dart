26436e6054fa956ea1888d3b411e2d6aa4fe2d27
Youssef <youssefabdallah2016@gmail.com>
2026-07-10 01:47:59 +0300

    create home screen body

--- lib/features/home/ui/home_screen.dart
+++ lib/features/home/ui/home_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/home/ui/widgets/home_screen_body.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(child: Center(child: Text("Home"))),
-    );
+    return Scaffold(body: SafeArea(child: HomeScreenBody()));
   }
 }
