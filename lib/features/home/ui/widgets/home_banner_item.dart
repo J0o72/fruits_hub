7be2e7b712bc7e6400b99e7707ad5ae197548da7
Youssef <youssefabdallah2016@gmail.com>
2026-07-10 20:13:57 +0300

    - create home banner item - create home banner button - add svg image

---
+++ lib/features/home/ui/widgets/home_banner_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+import 'package:flutter_svg/svg.dart' hide Svg;
+import 'package:fruit_hub/core/helpers/app_images.dart';
+import 'package:fruit_hub/core/helpers/spacing.dart';
+import 'package:fruit_hub/core/theme/text_styles.dart';
+import 'package:fruit_hub/features/home/ui/widgets/home_banner_button.dart';
+
+class HomeBannerItem extends StatelessWidget {
+  const HomeBannerItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 342 / 158,
+      child: SizedBox(
+        width: (MediaQuery.sizeOf(context).width - 32),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
+              bottom: 0,
+              right: (MediaQuery.sizeOf(context).width - 32) * 0.4,
+              child: SvgPicture.asset(
+                AppImages.onBoardingImagePageView2,
+                fit: BoxFit.fill,
+              ),
+            ),
+
+            Container(
+              width: (MediaQuery.sizeOf(context).width - 32) * 0.5,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: Svg('assets/SVGs/featured_item_background.svg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+
+              child: Padding(
+                padding: const EdgeInsets.only(right: 32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    verticalSpace(25),
+                    Text('عروض العيد', style: TextStyles.font13WhiteRegular),
+                    Spacer(),
+                    Text('خصم 25%', style: TextStyles.font19WhiteBold),
+                    verticalSpace(12),
+                    HomeBannerButton(),
+
+                    verticalSpace(29),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/ui/widgets/home_screen_body.dart
+++ lib/features/home/ui/widgets/home_screen_body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fruit_hub/core/helpers/spacing.dart';
 import 'package:fruit_hub/features/home/ui/widgets/home_app_bar.dart';
+import 'package:fruit_hub/features/home/ui/widgets/home_banner_item.dart';
 import 'package:fruit_hub/features/home/ui/widgets/search_text_field.dart';
 
 class HomeScreenBody extends StatelessWidget {
@@ -12,7 +13,14 @@ class HomeScreenBody extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w),
       child: Column(
-        children: [HomeAppBar(), verticalSpace(16), SearchTextField()],
+        children: [
+          HomeAppBar(),
+          verticalSpace(16),
+          SearchTextField(),
+          verticalSpace(12),
+
+          HomeBannerItem(),
+        ],
       ),
     );
   }
