c08fbeaae21e66a32e14a3429143e86d3f1d146d
Youssef <youssefabdallah2016@gmail.com>
2026-07-11 02:42:08 +0300

    display user name in home app bar

--- lib/features/home/ui/widgets/home_app_bar.dart
+++ lib/features/home/ui/widgets/home_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:fruit_hub/core/functions/get_user_data.dart';
 import 'package:fruit_hub/core/theme/app_colors.dart';
 import 'package:fruit_hub/core/theme/text_styles.dart';
 
@@ -14,7 +15,10 @@ class HomeAppBar extends StatelessWidget {
         child: Image.asset('assets/images/profile_image.png'),
       ),
       title: Text('صباح الخير !..', style: TextStyles.font16lightGrayRegular),
-      subtitle: Text('أحمد مصطفي', style: TextStyles.font16LightBlackBold),
+      subtitle: Text(
+        getUserData().name,
+        style: TextStyles.font16LightBlackBold,
+      ),
       trailing: CircleAvatar(
         backgroundColor: AppColors.mainLightestGreen,
         child: SvgPicture.asset('assets/SVGs/notification_icon.svg'),
