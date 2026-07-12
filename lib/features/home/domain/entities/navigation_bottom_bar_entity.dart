73ed17cb77be36fc8d715100d28329f9786c1e63
Youssef <youssefabdallah2016@gmail.com>
2026-07-12 03:48:45 +0300

    create entity for NavigationBottomBar

---
+++ lib/features/home/domain/entities/navigation_bottom_bar_entity.dart
@@ -0,0 +1,10 @@
+class NavigationBottomBarEntity {
+  final String activeImage, inActiveImage;
+  final String name;
+
+  NavigationBottomBarEntity({
+    required this.activeImage,
+    required this.inActiveImage,
+    required this.name,
+  });
+}
