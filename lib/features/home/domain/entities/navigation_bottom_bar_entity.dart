36a4481e7438c7d91caa34b253eda8e7dd053761
Youssef <youssefabdallah2016@gmail.com>
2026-07-12 04:00:30 +0300

    create method return list of NavigationBottomBarEntity

--- lib/features/home/domain/entities/navigation_bottom_bar_entity.dart
+++ lib/features/home/domain/entities/navigation_bottom_bar_entity.dart
@@ -7,4 +7,30 @@ class NavigationBottomBarEntity {
     required this.inActiveImage,
     required this.name,
   });
+
+  List<NavigationBottomBarEntity> get navigationBottomBarItems => [
+    NavigationBottomBarEntity(
+      activeImage: 'assets/SVGs/active-home-icon.svg',
+      inActiveImage: 'assets/SVGs/inactive-home-icon.svg',
+      name: 'الرئيسية',
+    ),
+
+    NavigationBottomBarEntity(
+      activeImage: 'assets/SVGs/active-products-icon.svg',
+      inActiveImage: 'assets/SVGs/inactive-products-icon.svg',
+      name: 'المنتجات',
+    ),
+
+    NavigationBottomBarEntity(
+      activeImage: 'assets/SVGs/active-shopping-cart-icon.svg',
+      inActiveImage: 'assets/SVGs/inactive-shopping-cart-icon.svg',
+      name: 'سلة التسوق',
+    ),
+
+    NavigationBottomBarEntity(
+      activeImage: 'assets/SVGs/active-profile-icon.svg',
+      inActiveImage: 'assets/SVGs/inactive-profile-icon.svg',
+      name: 'حسابي',
+    ),
+  ];
 }
