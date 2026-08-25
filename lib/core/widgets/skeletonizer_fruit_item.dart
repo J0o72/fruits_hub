a933a2359800031312e0130e7ae236f5f5c1ed22
Youssef <youssefabdallah2016@gmail.com>
2026-08-25 03:44:51 +0300

    edit SkeletonizerFruitItem widget

--- lib/core/widgets/skeletonizer_fruit_item.dart
+++ lib/core/widgets/skeletonizer_fruit_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/theme/app_colors.dart';
 import 'package:fruit_hub/core/theme/text_styles.dart';
 
 class SkeletonizerFruitItem extends StatelessWidget {
@@ -7,11 +8,12 @@ class SkeletonizerFruitItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.transparent),
-      width: double.infinity,
-      height: 500,
+      decoration: BoxDecoration(
+        color: AppColors.lightestGray,
+        borderRadius: BorderRadius.circular(4),
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
             decoration: BoxDecoration(color: Colors.transparent),
