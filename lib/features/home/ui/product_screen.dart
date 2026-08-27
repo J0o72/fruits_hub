07e3c3731a7c624f97f8a7748cb108c3bf08ab01
Youssef <youssefabdallah2016@gmail.com>
2026-08-27 18:04:09 +0300

    provide products cubit to products screen & trigger it into initState

--- lib/features/home/ui/product_screen.dart
+++ lib/features/home/ui/product_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/DI/dependancy_injection.dart';
+import 'package:fruit_hub/core/cubits/product/product_cubit.dart';
+import 'package:fruit_hub/core/repos/product_repo/product_repo.dart';
 import 'package:fruit_hub/features/home/ui/widgets/product_screen_body.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -6,6 +10,9 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ProductScreenBody();
+    return BlocProvider(
+      create: (context) => ProductCubit(productRepo: getIt<ProductRepo>()),
+      child: const ProductScreenBody(),
+    );
   }
 }
--- lib/features/home/ui/widgets/product_screen_body.dart
+++ lib/features/home/ui/widgets/product_screen_body.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/cubits/product/product_cubit.dart';
 import 'package:fruit_hub/core/helpers/spacing.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/home/ui/widgets/products_result_and_filtering_bar.dart';
 import 'package:fruit_hub/features/home/ui/widgets/search_text_field.dart';
 
-class ProductScreenBody extends StatelessWidget {
+class ProductScreenBody extends StatefulWidget {
   const ProductScreenBody({super.key});
+
+  @override
+  State<ProductScreenBody> createState() => _ProductScreenBodyState();
+}
+
+class _ProductScreenBodyState extends State<ProductScreenBody> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProductCubit>().getProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +35,8 @@ class ProductScreenBody extends StatelessWidget {
           SearchTextField(),
           verticalSpace(16),
           ProductsResultAndFilteringBar(),
+          verticalSpace(16),
+          // ProductsGridView(products: []),
         ],
       ),
     );
