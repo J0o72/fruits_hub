5e9e96051a10c971f18183738defa67a9faf78de
Youssef <youssefabdallah2016@gmail.com>
2026-09-02 00:59:44 +0300

    add two method (isProductExist , getCartItem ) into CartEntity that help me to add product in cubit

--- lib/features/home/domain/entities/cart_entity.dart
+++ lib/features/home/domain/entities/cart_entity.dart
@@ -1,7 +1,27 @@
+import 'package:fruit_hub/core/entities/product_entity.dart';
 import 'package:fruit_hub/features/home/domain/entities/cart_item_entity.dart';
 
 class CartEntity {
   final List<CartItemEntity> cartItems;
 
   CartEntity({required this.cartItems});
+
+  bool isProductExist(ProductEntity product) {
+    for (var cartItem in cartItems) {
+      if (cartItem.productEntity == product) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  CartItemEntity getCartItem(ProductEntity product) {
+    for (var cartItem in cartItems) {
+      if (cartItem.productEntity == product) {
+        return cartItem;
+      }
+    }
+
+    return CartItemEntity(productEntity: product, quantity: 1);
+  }
 }
