142dd92e9b2ad90e037c2ef444f33cbc8c39dc9b
Youssef <youssefabdallah2016@gmail.com>
2026-09-02 00:53:20 +0300

    create cart states & its cubit

---
+++ lib/features/home/logic/cart/cart_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/features/home/logic/cart/cart_state.dart';
+
+class CartCubit extends Cubit<CartState> {
+  CartCubit(super.initialState);
+}
