df97fc223e4574c0b8d8503ff7145fd131d930d4
Youssef <youssefabdallah2016@gmail.com>
2026-09-02 01:43:41 +0300

    pass cart items from cubit to CartItemsList view

--- lib/features/home/ui/widgets/cart_screen_body.dart
+++ lib/features/home/ui/widgets/cart_screen_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/helpers/spacing.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/features/home/logic/cart/cart_cubit.dart';
 import 'package:fruit_hub/features/home/ui/widgets/cart_header_products_number.dart';
 import 'package:fruit_hub/features/home/ui/widgets/cart_items_list.dart';
 import 'package:fruit_hub/features/home/ui/widgets/custom_divider.dart';
@@ -30,7 +32,9 @@ class CartScreenBody extends StatelessWidget {
           ),
         ),
         SliverToBoxAdapter(child: CustomDivider()),
-        CartItemsList(cartItems: []),
+        CartItemsList(
+          cartItems: context.watch<CartCubit>().cartEntity.cartItems,
+        ),
         SliverToBoxAdapter(child: CustomDivider()),
       ],
     );
