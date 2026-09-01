666cfaad6df9fc97c6fdc1ca63176349845e0bc2
Youssef <youssefabdallah2016@gmail.com>
2026-09-02 01:32:28 +0300

    use BlocListener to show snack bar when add product to cart succesfully

--- lib/features/home/ui/main_screen.dart
+++ lib/features/home/ui/main_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/functions/show_custom_snack_bar.dart';
 import 'package:fruit_hub/features/home/logic/cart/cart_cubit.dart';
+import 'package:fruit_hub/features/home/logic/cart/cart_state.dart';
 import 'package:fruit_hub/features/home/ui/widgets/custom_navigation_bottom_bar.dart';
 import 'package:fruit_hub/features/home/ui/widgets/main_screen_body.dart';
 
@@ -25,7 +27,19 @@ class _MainScreenState extends State<MainScreen> {
             setState(() {});
           },
         ),
-        body: SafeArea(child: MainScreenBody(currentViewIndex: currentIndex)),
+        body: SafeArea(
+          child: BlocListener<CartCubit, CartState>(
+            listener: (context, state) {
+              if (state is CartItemAdded) {
+                showCustomSnackBar(context, 'تمت العملية بنجاح');
+              }
+              if (state is CartItemRemoved) {
+                showCustomSnackBar(context, 'تم حذف العنصر بنجاح');
+              }
+            },
+            child: MainScreenBody(currentViewIndex: currentIndex),
+          ),
+        ),
       ),
     );
   }
