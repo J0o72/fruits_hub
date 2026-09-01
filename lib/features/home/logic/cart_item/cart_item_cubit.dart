60f19095274636de188c061db03e0c34fb34ef64
Youssef <youssefabdallah2016@gmail.com>
2026-09-02 02:33:54 +0300

    add method (updateCartItem) to cart item cubit

--- lib/features/home/logic/cart_item/cart_item_cubit.dart
+++ lib/features/home/logic/cart_item/cart_item_cubit.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fruit_hub/features/home/logic/cart/cart_state.dart';
+import 'package:fruit_hub/features/home/domain/entities/cart_item_entity.dart';
+import 'package:fruit_hub/features/home/logic/cart_item/cart_item_state.dart';
 
-class CartItemCubit extends Cubit<CartState> {
-  CartItemCubit() : super(CartInitial());
+class CartItemCubit extends Cubit<CartItemState> {
+  CartItemCubit({required this.cartItemEntity}) : super(CartItemInitial());
+
+  final CartItemEntity cartItemEntity;
+
+  void updateCartItem(CartItemEntity cartItem) {
+    emit(CartItemUpdated());
+  }
 }
