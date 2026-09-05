c237827961179f86e488fb3c9dfc7617e2674ec3
Youssef <youssefabdallah2016@gmail.com>
2026-09-06 02:27:49 +0300

    navigate to checkout screen

--- lib/features/home/ui/widgets/cart_custom_button.dart
+++ lib/features/home/ui/widgets/cart_custom_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/helpers/extensions.dart';
+import 'package:fruit_hub/core/routing/routes.dart';
 import 'package:fruit_hub/core/widgets/app_text_button.dart';
 import 'package:fruit_hub/features/home/logic/cart/cart_cubit.dart';
 import 'package:fruit_hub/features/home/logic/cart_item/cart_item_cubit.dart';
@@ -13,7 +15,9 @@ class CustomCartButton extends StatelessWidget {
     return BlocBuilder<CartItemCubit, CartItemState>(
       builder: (context, state) {
         return AppTextButton(
-          onPressed: () {},
+          onPressed: () {
+            context.pushNamed(Routes.checkoutScreen);
+          },
           text:
               'الدفع ${context.watch<CartCubit>().cartEntity.calculateTotalPrice()} جنية',
         );
