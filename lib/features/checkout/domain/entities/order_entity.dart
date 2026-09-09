9327535782bcb15250bb0a834bac29660e1c7be0
Youssef <youssefabdallah2016@gmail.com>
2026-09-10 01:06:57 +0300

    create OrderEntity and ShippingAddressEntity

---
+++ lib/features/checkout/domain/entities/order_entity.dart
@@ -0,0 +1,14 @@
+import 'package:fruit_hub/features/checkout/domain/entities/shipping_address_entity.dart';
+import 'package:fruit_hub/features/home/domain/entities/cart_entity.dart';
+
+class OrderEntity {
+  final List<CartEntity> cartItems;
+  final bool payWithCash;
+  final ShippingAddressEntity shippingAddressEntity;
+
+  OrderEntity({
+    required this.cartItems,
+    required this.payWithCash,
+    required this.shippingAddressEntity,
+  });
+}
---
+++ lib/features/checkout/domain/entities/shipping_address_entity.dart
@@ -0,0 +1,17 @@
+class ShippingAddressEntity {
+  final String name;
+  final String phone;
+  final String address;
+  final String city;
+  final String floor;
+  final String email;
+
+  ShippingAddressEntity({
+    required this.name,
+    required this.phone,
+    required this.address,
+    required this.city,
+    required this.floor,
+    required this.email,
+  });
+}
