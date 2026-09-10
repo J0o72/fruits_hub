941ad58939c2beaadfd6b9d3ca70912af72d474c
Youssef <youssefabdallah2016@gmail.com>
2026-09-10 03:08:13 +0300

    use Automatic Keep Alive Client Mixin with shipping section to save what i chosen

--- lib/features/checkout/ui/widgets/shipping_section.dart
+++ lib/features/checkout/ui/widgets/shipping_section.dart
@@ -12,11 +12,13 @@ class ShippingSection extends StatefulWidget {
   State<ShippingSection> createState() => _ShippingSectionState();
 }
 
-class _ShippingSectionState extends State<ShippingSection> {
+class _ShippingSectionState extends State<ShippingSection>
+    with AutomaticKeepAliveClientMixin {
   int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Column(
       children: [
         verticalSpace(32),
@@ -54,4 +56,7 @@ class _ShippingSectionState extends State<ShippingSection> {
       ],
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
